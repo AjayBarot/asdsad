24f0062502a6c770beeba569785e28d6d8cb0cfe
AjayBarot-BTC <ajay.barot@beaverlogic.com>
2014-06-26 12:13:18 +0530

    initiale message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AptanaDemo::Application.config.secret_key_base = '04bf5e91e2035b7922036b9e3704ca75d50e21a57af7e1b2b304b47fa6956352078a90c62c3acf459abe655ddf3b73f90af3e27d026ae1a319f3d9fe34351c2b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AptanaDemo::Application.config.session_store :cookie_store, key: '_aptana_demo_session'
